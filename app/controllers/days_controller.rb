8d43dc91c58b51ef030c88931a4b0654d3e2f7cb
Danpowers24 <danpowers610@gmail.com>
2020-04-02 13:55:04 -0400

    Fix syntax and spacing in app/controllers/days_controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,5 +1,5 @@
 class DaysController < ApplicationController
-  before_action :set_day, only: [:show, :update, :destroy]
+  before_action :set_day, only: %i[show update destroy]
 
   # GET /days
   def index
@@ -39,13 +39,14 @@ class DaysController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_day
-      @day = Day.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def day_params
-      params.require(:day).permit(:date, :pain_level, :notes)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_day
+    @day = Day.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def day_params
+    params.require(:day).permit(:date, :pain_level, :notes)
+  end
 end
