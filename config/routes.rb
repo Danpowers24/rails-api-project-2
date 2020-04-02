84415f7efbc57fcfef2b2af44b069957a5d4b189
Danpowers24 <danpowers610@gmail.com>
2020-04-02 13:56:53 -0400

    Write routes for days in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :user, except: %i[new edit]
   resources :days, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
+
+  get '/days' => 'days#index'
+  get '/days/:id' => 'days#show'
+  delete '/days/:id' => 'days#destroy'
+  patch '/days/:id' => 'days#update'
+  post '/days' => 'days#create'
 
   # Custom routes
   post '/sign-up' => 'users#signup'
